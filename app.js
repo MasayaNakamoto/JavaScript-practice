dc989c4997789bd3b40a0fb79c5a76ccea2d8b1c
USER <user@MasayaNakamoto.local>
2021-06-23 18:29:53 +0900

    5-finish

--- app.js
+++ app.js
@@ -2,9 +2,18 @@
 //   $('body').html('<h1>Hello jQuery!!</h1>');
 // });
 
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// $(function(){
+//   $('.box1').slideUp();
+// });
+
 $(function(){
-  $('.box1').css({
-    'background-color': '#0000FF',
-    'height': '100px'
-  });
+  $('.box1').show();
+  $('.box1').css({'background-color': '#0000FF'});
 });
